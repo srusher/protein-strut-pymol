87ace7ff0c29b0dce8c573592c19e8a7b8c88ffb
Nicholas DeBouver <89560196+scatteredBl00p@users.noreply.github.com>
2021-10-24 22:46:00 -0700

    Add files via upload

---
+++ strut_finder.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 24 22:09:38 2021
+
+@author: nd
+"""
+
+#import scripts
+
+#import libraries/modules
+import os
+
+
+## define global variables
+## may need to change command depending on dssp configuration
+# file_pdb = sys.argv[1]
+file_pdb = "6xmy.pdb"
+dssp_out = "dssp_output.txt"
+command = "mkdssp --output-format=dssp "
+
+def main():
+    os.system(command + file_pdb + " > " + dssp_out)
+    os.system("python3 sec_strut.py " + dssp_out)
+
+main()
